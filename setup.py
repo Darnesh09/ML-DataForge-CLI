6b2ac03bf535c251b1a6270a69c4493b3c5c11f3
Darnesh09 <darnesh.d.p2002@gmail.com>
2023-08-14 18:10:38 +0530

    final commit

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import find_packages,setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    """
+        This file read the requirements from the file_path
+    """
+    requirements = []
+    with open(file_path) as file:
+        requirements = [req.strip() for req in file.readlines()]
+        if '-e .' in requirements:
+            requirements.remove('-e .')
+    return requirements
+
+
+setup(
+    name = 'ML DataForgr',
+    packages = find_packages(),
+    install_requires = get_requirements("requirements.txt")
+)
